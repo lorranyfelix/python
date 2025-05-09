fa6d879c82d50db9b54ac4f967459eb06e044298
gabriella <gabriella.barbosa.souza@escola.pr.gov.br>
2025-05-09 12:14:05 -0300

    exercicio 2

---
+++ capitulo 2/exercicio2.py
@@ -0,0 +1,11 @@
+capa = 24.95 * 0.60
+
+transporte = 3
+adicional = 0.75
+
+quantidadeCopias = 60
+
+custoTotal = ((capa * quantidadeCopias) + (transporte + ( 59 * adicional)))
+
+print(custoTotal)
+
