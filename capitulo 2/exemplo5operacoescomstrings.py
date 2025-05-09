7f099613cf2d9c99320a0272c0a9f0b5bd77844f
gabriella <gabriella.barbosa.souza@escola.pr.gov.br>
2025-05-09 11:19:05 -0300

    operações com string

---
+++ capitulo 2/exemplo5operacoescomstrings.py
@@ -0,0 +1,12 @@
+# É proibido utilizar operações matemáticas com strings
+# Exceto em 2 casos
+
+first = 'Taylor'
+second = ' Swift'
+nomeSobrenome = first + second # o + aqui significa concatenação
+
+print(nomeSobrenome)
+
+stringQualquer = "Teste " * 3
+
+print(stringQualquer)
