343c83244351dccd91fee1a2bdf7d6551aabbb76
gabriella <gabriella.barbosa.souza@escola.pr.gov.br>
2025-05-09 12:24:26 -0300

    exercicio 3

---
+++ capitulo 2/exercicio3.py
@@ -0,0 +1,9 @@
+horarioSairCasa = 06.52
+
+certoPasso = 08.15
+
+passoMaisRapido = 07.12
+
+horarioChegueiCasa = horarioSairCasa  (certoPasso + (passoMaisRapido * 3) + certoPasso)
+
+print(horarioChegueiCasa)
