b0334b835391816a026731bb85d9cce33618d91a
rafael mudrei <rafael.mudrei@escola.pr.gov.br>
2025-05-16 09:55:24 -0300

    exercicio 4

--- capitulo 2/exercicio3.py
+++ capitulo 2/exercicio3.py
@@ -1,9 +1,20 @@
-horarioSairCasa = 06.52
+print("valor do primeiro: 8min e 15s")
+minutoPrimeiroTrecho = 8
+segundosPrimeiroTrecho = 15
+totalTempoPrimeiroTrecho = (8 * 60) + segundosPrimeiroTrecho
+print("Tempo do primeiro trecho em segundos:",totalTempoPrimeiroTrecho)
 
-certoPasso = 08.15
+print("Tempo do segundo trecho: 7min e 12s ")
+minutoSegundoTrecho = 7
+segundosSegundoTrecho = 12
+totalTempoSegundoTrecho = (7 * 60) + segundosSegundoTrecho
+print("Tempo do segundo trecho em segundos:", totalTempoSegundoTrecho)
 
-passoMaisRapido = 07.12
+print("valor do terceiro: 8min e 15s")
+minutoTerceiroTrecho = 8
+segundosTerceiroTrecho = 15
+totalTempoTerceiroTrecho = (8 * 60) + segundosTerceiroTrecho
+print("Tempo do primeiro trecho em segundos:",totalTempoTerceiroTrecho)
 
-horarioChegueiCasa = horarioSairCasa  (certoPasso + (passoMaisRapido * 3) + certoPasso)
 
-print(horarioChegueiCasa)
+
