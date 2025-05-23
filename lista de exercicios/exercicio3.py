34ac1d9991dc8388e0995a2886d3ace5c00e5e82
endrius <endrius.pontes@escola.pr.gov.br>
2025-05-23 11:21:29 -0300

    lista de exercicio

--- lista de exercicios/exercicio3.py
+++ lista de exercicios/exercicio3.py
@@ -2,7 +2,7 @@ raioCirculo = int(input("Digite o raio do círculo"))
 pi = 3.14159
 diametroCirculo = 2 * raioCirculo
 areaCirculo = (pi) * raioCirculo ** 2
-circuferenciaCirculo = 2 * pi
+circuferenciaCirculo = 2 * pi * raioCirculo
 
 print("O diâmetro do círculo é", diametroCirculo)
 print("A circuferência do círculo é", circuferenciaCirculo)
