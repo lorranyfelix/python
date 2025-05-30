eb8243892cf781ad3c2088de38858f91ba7de51a
endrius <endrius.pontes@escola.pr.gov.br>
2025-05-30 11:42:13 -0300

    exemplo 4

---
+++ capitulo 3/exemplo4.py
@@ -0,0 +1,10 @@
+def mostrar_nome_inteiro(nome, sobreNome):
+    print(nome)
+    sobrenome(sobreNome)
+
+def sobrenome(sobrenom):
+    print(sobreNome)
+
+nome = "José"
+sobreNome = "Valim"
+mostrar_nome_inteiro(nome, sobreNome)
