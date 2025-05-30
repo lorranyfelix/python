7e6c226ff3848c2956d120ea9f516ad8424069fe
endrius <endrius.pontes@escola.pr.gov.br>
2025-05-30 12:36:02 -0300

    exercicio 1

---
+++ capitulo 3/exercicios/exercicio1.py
@@ -0,0 +1,7 @@
+def justificar_direita(stringQualquer):
+    setentaEspacos = "          "
+    print(setentaEspacos*7 + stringQualquer)
+
+stringQualquer = "Lorrany"
+justificar_direita(stringQualquer)
+
