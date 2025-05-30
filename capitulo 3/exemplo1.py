64b7c3b90d9eca164d4bd25727666982d23925de
endrius <endrius.pontes@escola.pr.gov.br>
2025-05-30 09:58:49 -0300

    exemplo 1

---
+++ capitulo 3/exemplo1.py
@@ -0,0 +1,10 @@
+# A função int converte um número em inteiro
+string = '32'
+print(type(string))
+print(type(int('32')))
+
+
+int('2')
+#int('hello') erro por que a função int() não testa caraacteres que não seja numéricos
+#algo seguido de parêntes é uma função
+
