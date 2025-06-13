a78612192ae08f2895153a157fa2dbb59926e5e0
endrius <endrius.pontes@escola.pr.gov.br>
2025-06-13 11:11:55 -0300

    exercicios exemplo 2

--- capitulo 3/exercicios exemplo/exercicio1.py
+++ capitulo 3/exercicios exemplo/exercicio1.py
@@ -1,3 +1,20 @@
-numero = int(input("Digite 1º número"))
-numero2 = int(input("Digite o 2º número"))
+def soma(numero, numero2):
+    print("o resultado da adição é:", numero + numero2)
 
+def subtracao(numero, numero2):
+    print("o resultado da subtração é:", numero - numero2)
+
+def multiplicacao(numero, numero2):
+    print("o resultado da multiplicação é:", numero * numero2)
+
+def divisao(numero, numero2):
+    resultado = numero / numero2
+    print("o resultado da divisão é:", resultado)
+
+numero = int(input("Digite 1º número:"))
+numero2 = int(input("Digite o 2º número:"))
+
+soma(numero, numero2)
+subtracao (numero, numero2)
+multiplicacao(numero, numero2)
+divisao(numero, numero2)
