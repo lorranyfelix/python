f4c99269db8253f1dca519672523aa2872315cd3
Andrey <Andrey.schmitz@escola.pr.gov.br>
2025-09-26 11:25:55 -0300

    correção

--- capitulo 5/estrutura de decisão/exercicio3.py
+++ capitulo 5/estrutura de decisão/exercicio3.py
@@ -1,16 +1,15 @@
 
 def comparacaoNumeros (numero1, numero2, numero3):
+
     if (numero1 > numero2):
         if (numero1 > numero3):
-            print(f"O número {numero1} é o maior número")
+            print(f"O primeiro número é o maior número")
+    elif (numero2 > numero3):
+            print(f"O segundo número é o maior número")
+    else:
+        print("O terceiro número é o maior número")
 
-    if (numero2 > numero1):
-        if (numero2 > numero3):
-            print("O", numero2, "é o maior numero")
 
-    if (numero3 > numero1):
-        if (numero3 > numero2):
-            print("O", numero3, "é o maior numero")
 
 numero1 = int(input("Digite um número"))
 numero2 = int(input("Digite outro número"))
